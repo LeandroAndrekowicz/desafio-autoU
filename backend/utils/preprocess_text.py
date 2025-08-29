476bd16baba12bb4a6e86c455b207778fe1ea6c2
Leandro Andrekowicz <103007824+LeandroAndrekowicz@users.noreply.github.com>
2025-08-28 22:11:41 -0300

    Correção de importação

--- backend/utils/preprocess_text.py
+++ backend/utils/preprocess_text.py
@@ -3,13 +3,14 @@ nltk.data.path.append("/opt/render/project/src/.venv/nltk_data")
 
 from nltk.corpus import stopwords
 import nltk
+import string
 
 try:
     STOPWORDS = set(stopwords.words("portuguese"))
 except LookupError:
     nltk.download("stopwords")
     STOPWORDS = set(stopwords.words("portuguese"))
-
+    
 class PreprocessText:
     @staticmethod
     def preprocess_text(text: str) -> str:
